7d54487c95780eff471193d4eb707015966279d2
up here <iam@upwhere.me>
2013-08-05 09:26:45 +0000

    module fallback and last-resort manual screenshotting (for linux, at least)

--- pixels.py
+++ pixels.py
@@ -6,7 +6,19 @@ A lazy Sunday afternoon hack!
 import time
 
 import Image
-import pyscreenshot as ImageGrab
+manualgrab = "" # false, for now.
+try:
+    import ImageGrab
+except ImportError:
+    try:
+        import pyscreenshot as ImageGrab # fall back on pyscreenshot, cross-platform.
+    except ImportError:
+        import os
+        linuxTmpFilename = "/tmp/Pixels.onepixel.png" # we have to use an intermediate file when no appropriate libraries are loadable, write to /tmp/, the ramdisk, to prevent unneccesary disk activity.
+        manualgrab = "import -silent -window root %s" % linuxTmpFilename # the OS command to create a screenshot file ( -silent: don't ring the system bell on each screenshot, -window root: capture the entire screen, not just the active window )
+
+
+
 
 
 X = 1 # seconds to pause before taking next pixel
@@ -21,7 +33,12 @@ if __name__ == '__main__':
     pixels = pixels_img.load()
     
     for w in xrange(WIDTH):
-        img = ImageGrab.grab() # take a screenshot
+        if not manualgrab:
+            img = ImageGrab.grab() # take a screenshot
+        else:
+            os.system(manualgrab)
+            img = Image.open(linuxTmpFilename)
+
         img = img.resize((WIDTH, HEIGHT)) # create a thumbnail
         for h in xrange(HEIGHT):
             pixels[w, h] = img.getpixel((w, h)) # pixels(x, y) = (r, g, b)
